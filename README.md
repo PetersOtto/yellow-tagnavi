bcccd808f9f38dc971518e4fb569ac4aa3f1b2be
PetersOtto <52236754+PetersOtto@users.noreply.github.com>
2023-02-24 11:33:06 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # yellow-tagnavi
-This extension creates a navigation from the blogtags
+Extension for Datenstrom Yellow. It sets a link that looks like an embedded video.
+
+<p align="center"><img src="screenshot-tagnavi.jpg?raw=true" alt="Bildschirmfoto"></p>
+
+## How to use the Yellow tagNavi extension
+
+1. Insert the following code in a suitable place in the start-blog.html.
+<?php $url = $this->yellow->page->getBase(). "/" .$this->yellow->toolbox->getLocationArguments(); echo $this->yellow->extension->get("tagnavi")->getTagNavi("/" , "0" , "tagnavi" , "All Projects" , $url)?>
+The following parameters can be adjusted:
+getTagNavi("1" , "2" , "3" , "4" , $url)?>
+1 = location of blog start page (/ or /blog/ or ...)
+2 = number of entries to show per shortcut, 0 for unlimited
+3 = Name for the css class
+4 = Name for the link that displays all blog entries. (All or All Projects or ...)
+
+2. Make adjustments in css or scss. Here is an scss example:
+
+.tagnavi{
+  ul{
+    padding-left: 0;
+  }
+  li{
+    display: inline-block;
+    padding-right: 2rem;
+  }
+  a{
+    text-decoration: none;
+    &:hover{
+      text-decoration: underline;
+    }
+  }
+  a.active{
+    text-decoration: underline;
+  }
+}
+
+3. This extension requires the blog extension.
+
+
+## Installation
+
+[Download extension](https://github.com/PetersOtto/yellow-tagnavi/archive/refs/heads/main.zip) and copy zip file into your `system/extensions` folder. Right click if you use Safari.
+
+## Developer
+
+PetersOtto. [Get help](https://datenstrom.se/yellow/help/)
---
+++ tagnavi.php
@@ -0,0 +1,54 @@
+<?php
+class YellowTagNavi
+{
+    const VERSION = "0.8.01";
+    public $yellow; // access to API
+
+    // Handle initialisation
+    public function onLoad($yellow)
+    {
+        $this->yellow = $yellow;
+    }
+
+    // Return a blogtag navigation
+    public function getTagNavi($startLocation, $entriesMax, $class, $filterName, $url)
+    {
+        $output = null;
+        $blogStart = $this->yellow->content->find($startLocation);
+        $pages = $this->yellow->extension->get("blog")->getBlogPages($startLocation);
+        $tags = $this->yellow->extension->get("blog")->getMeta($pages, "tag");
+
+        if (!is_array_empty($tags)) {
+            $tags = $this->yellow->lookup->normaliseArray($tags);
+            if ($entriesMax != 0 && count($tags) > $entriesMax) {
+                uasort($tags, "strnatcasecmp");
+                $tags = array_slice($tags, -$entriesMax, $entriesMax, true);
+            }
+            uksort($tags, "strnatcasecmp");
+            $output = "<div class=\"" . htmlspecialchars($class) . "\">\n";
+            $output .= "<ul>\n";
+            if ($url == $blogStart->getLocation(true)){
+                $output .= "<li><a class=\"active\" aria-current=\"page\" href=\"" . $blogStart->getLocation(true) . "\">";
+                $output .= htmlspecialchars($filterName) . "</a></li>\n";
+            }else{
+                $output .= "<li><a href=\"" . $blogStart->getLocation(true) . "\">";
+                $output .= htmlspecialchars($filterName) . "</a></li>\n";
+            }
+            foreach ($tags as $key => $value) {
+                $urlNavi = $blogStart->getLocation(true) . $this->yellow->lookup->normaliseArguments("tag:$key");
+                if ($url == $urlNavi){
+                    $output .= "<li><a class=\"active\" aria-current=\"page\" href=\"" . $urlNavi . "\">";
+                    $output .= htmlspecialchars($key) . "</a></li>\n";
+                } else {
+                    $output .= "<li><a href=\"" . $urlNavi . "\">";
+                    $output .= htmlspecialchars($key) . "</a></li>\n";
+                }
+            }
+            $output .= "</ul>\n";
+            $output .= "</div>\n";
+        } else {
+            $page->error(500, "Blogtags '$startLocation' does not exist!");
+        }
+        return $output;
+    }
+}
