cf0261d0b8dfe5713d38a2dbdaac4b5427bbf0fe
PetersOtto <52236754+PetersOtto@users.noreply.github.com>
2023-02-28 18:55:01 +0100

    Update tagnavi.php

--- tagnavi.php
+++ tagnavi.php
@@ -18,6 +18,14 @@ class YellowTagNavi
         $pages = $this->yellow->extension->get("blog")->getBlogPages($startLocation);
         $tags = $this->yellow->extension->get("blog")->getMeta($pages, "tag");
         $url = $url.$startLocation.$urlArg;
+        
+        if (strpos($urlArg, "age:")) {
+            $pageNr = explode("page:", $urlArg);
+            $urlPagination = "page:" . $pageNr[1];
+        } else{
+            $urlPagination = "";
+        }
+        
         if (!is_array_empty($tags)) {
             $tags = $this->yellow->lookup->normaliseArray($tags);
             if ($entriesMax != 0 && count($tags) > $entriesMax) {
@@ -25,27 +33,23 @@ class YellowTagNavi
                 $tags = array_slice($tags, -$entriesMax, $entriesMax, true);
             }
             uksort($tags, "strnatcasecmp");
-            $output = "<div class=\"" . htmlspecialchars($class) . "\">\n";
-            $output .= "<ul>\n";
-            if ($url == $blogStart->getLocation(true)){
-                $output .= "<li><a class=\"active\" aria-current=\"page\" href=\"" . $blogStart->getLocation(true) . "\">";
+            if ($url == $blogStart->getLocation(true). $urlPagination){
+                $output .= "<li class=\"nav-item\"><a class=\"nav-link active\" aria-current=\"page\" href=\"" . $blogStart->getLocation(true) . "\">";
                 $output .= htmlspecialchars($filterName) . "</a></li>\n";
             }else{
-                $output .= "<li><a href=\"" . $blogStart->getLocation(true) . "\">";
+                $output .= "<li class=\"nav-item\"><a class=\"nav-link\" href=\"" . $blogStart->getLocation(true) . "\">";
                 $output .= htmlspecialchars($filterName) . "</a></li>\n";
             }
             foreach ($tags as $key => $value) {
                 $urlNavi = $blogStart->getLocation(true) . $this->yellow->lookup->normaliseArguments("tag:$key");
-                if ($url == $urlNavi){
-                    $output .= "<li><a class=\"active\" aria-current=\"page\" href=\"" . $urlNavi . "\">";
+                if ($url == $urlNavi.$urlPagination){
+                    $output .= "<li class=\"nav-item\"><a class=\"nav-link active\" aria-current=\"page\" href=\"" . $urlNavi . "\">";
                     $output .= htmlspecialchars($key) . "</a></li>\n";
                 } else {
-                    $output .= "<li><a href=\"" . $urlNavi . "\">";
+                    $output .= "<li class=\"nav-item\" ><a class=\"nav-link\" href=\"" . $urlNavi . "\">";
                     $output .= htmlspecialchars($key) . "</a></li>\n";
                 }
             }
-            $output .= "</ul>\n";
-            $output .= "</div>\n";
         } else {
             $output = "The location of your blog start page is wrong (/ or /blog/ or ...)";
         }
