02746d88574b1b540c38567e3ae6ff076becd6ec
PetersOtto <52236754+PetersOtto@users.noreply.github.com>
2024-02-15 14:13:13 +0100

    Update tagnavi.php

--- tagnavi.php
+++ tagnavi.php
@@ -1,7 +1,7 @@
 <?php
 class YellowTagNavi
 {
-    const VERSION = "0.8.04";
+    const VERSION = "0.8.05";
     public $yellow; // access to API
 
     // Handle initialisation
@@ -11,7 +11,7 @@ class YellowTagNavi
     }
 
     // Return a blogtag navigation
-    public function getTagNavi($startLocation, $entriesMax, $class, $filterName, $url, $urlArg)
+    public function getTagNavi($startLocation, $tagCount, $entriesMax, $class, $filterName, $url, $urlArg)
     {
         $output = null;
         $blogStart = $this->yellow->content->find($startLocation);
@@ -20,6 +20,8 @@ class YellowTagNavi
         // $tags = $pages->group("tag", false); // = group content by tag with descending name, Z-A
         // $tags = $pages->group("tag", false, "count"); // = group content by tag with descending count, highest first
         $url = $url.$startLocation.$urlArg;
+        $numberAllPosts = "";
+        $numberPosts = "";
         
         if (strpos($urlArg, "age:")) {
             $pageNr = explode("page:", $urlArg);
@@ -29,30 +31,40 @@ class YellowTagNavi
         }
         
         if (!is_array_empty($tags)) {
+            if ($tagCount == "count"){
+                $countValue = "";
+                foreach ($tags as $key => $value) {
+                    $countValue += count($value);
+                }
+                $numberAllPosts = "<span>" . $countValue . "</span>";
+            }
             if ($entriesMax != 0 && count($tags) > $entriesMax) {
                 $tags = array_slice($tags, -$entriesMax, $entriesMax, true);
             }
-            $output = "<div class=\"" . htmlspecialchars($class) . "\">\n";
+            $output = "<nav class=\"" . htmlspecialchars($class) . "\">\n";
             $output .= "<ul>\n";
             if ($url == $blogStart->getLocation(true). $urlPagination){
                 $output .= "<li><a class=\"active\" aria-current=\"page\" href=\"" . $blogStart->getLocation(true) . "\">";
-                $output .= htmlspecialchars($filterName) . "</a></li>\n";
+                $output .= htmlspecialchars($filterName) . "</a>" . $numberAllPosts . "</li>\n";
             }else{
                 $output .= "<li><a href=\"" . $blogStart->getLocation(true) . "\">";
-                $output .= htmlspecialchars($filterName) . "</a></li>\n";
+                $output .= htmlspecialchars($filterName) . "</a>" . $numberAllPosts . "</li>\n";
             }
             foreach ($tags as $key => $value) {
+                if ($tagCount == "count"){
+                    $numberPosts = "<span>" . count($value) . "</span>";
+                }
                 $urlNavi = $blogStart->getLocation(true) . $this->yellow->lookup->normaliseArguments("tag:$key");
                 if ($url == $urlNavi.$urlPagination){
                     $output .= "<li><a class=\"active\" aria-current=\"page\" href=\"" . $urlNavi . "\">";
-                    $output .= htmlspecialchars($key) . "</a></li>\n";
+                    $output .= htmlspecialchars($key) . "</a>" . $numberPosts . "</li>\n";
                 } else {
                     $output .= "<li><a href=\"" . $urlNavi . "\">";
-                    $output .= htmlspecialchars($key) . "</a></li>\n";
+                    $output .= htmlspecialchars($key) . "</a>" . $numberPosts . "</li>\n";
                 }
             }
             $output .= "</ul>\n";
-            $output .= "</div>\n";
+            $output .= "</nav>\n";
         } else {
             $output = "The location of your blog start page is wrong (/ or /blog/ or ...)";
         }
